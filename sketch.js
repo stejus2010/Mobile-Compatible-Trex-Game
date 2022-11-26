830e4881c6dd79b267c6eed88340c9cf1a7f0a13
stejus2010 <115551989+stejus2010@users.noreply.github.com>
2022-11-26 13:15:20 +0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -132,7 +132,7 @@ function draw() {
     obstaclesGroup.setLifetimeEach(-1);
     cloudsGroup.setLifetimeEach(-1);
     
-    if(touches.length>0 || mousePressedOver(restart) {      
+    if(touches.length>0 || mousePressedOver(restart)) {      
       reset();
       touches = []
     }
